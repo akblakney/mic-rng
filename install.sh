d1e48c5f6a2e08eb9dbecb11ab71369212267661
Adam Blakney <akblakney@gmail.com>
2022-11-28 12:53:46 -0500

    installer now generates the run script and sets a symbolic link in /usr/local/bin

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install dependencies
+#sudo apt install python3-pyaudio
+
+# generate run script
+touch ./run.sh
+echo '#!/bin/bash' > run.sh
+chmod u+x run.sh
+echo "python3 $(pwd)/rng.py \"\$@\" 2> /dev/null" >> run.sh
+
+# create symbolic link in /usr/local/bin
+sudo ln -s $(pwd)/run.sh /usr/local/bin/mic-rng
