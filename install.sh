2f1aee7c0a42fc91cef3ea7511a58d1768b0181d
Adam Blakney <akblakney@gmail.com>
2022-12-02 19:34:29 -0500

    added message at end of install script

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ echo "python3 $(pwd)/rng.py \"\$@\" 2> /dev/null" >> run.sh
 
 # create symbolic link in /usr/local/bin
 sudo ln -s $(pwd)/run.sh /usr/local/bin/mic-rng
+
+echo "symbolic link created in /usr/local/bin"
+echo "now run with mic-rng"
